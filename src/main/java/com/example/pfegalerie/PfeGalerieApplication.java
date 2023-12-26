67a5b753122dfbbd7091bf5ce8762d305588dc98
ahmed <ahmed.zaafrane@esprit.tn>
2023-12-26 18:34:16 +0100

    Initial commit

---
+++ src/main/java/com/example/pfegalerie/PfeGalerieApplication.java
@@ -0,0 +1,13 @@
+package com.example.pfegalerie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfeGalerieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PfeGalerieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pfegalerie/PfeGalerieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pfegalerie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfeGalerieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
