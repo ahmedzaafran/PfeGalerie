4ec26e48a7544961ed000aab5cab0790d9fab1ca
ahmed <ahmed.zaafrane@esprit.tn>
2023-12-27 14:54:21 +0100

    congif base de donne et lancement de projet (run ok)

--- src/main/java/com/example/pfegalerie/PfeGalerieApplication.java
+++ src/main/java/com/example/pfegalerie/PfeGalerieApplication.java
@@ -2,9 +2,10 @@ package com.example.pfegalerie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PfeGalerieApplication {
+public class PfeGalerieApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PfeGalerieApplication.class, args);
